d85cc24d82fe4a457ffbb464e47d00be9c10e860
NATANAEL DIDIK SUSANTO <45550958+natanaeldidiks@users.noreply.github.com>
2019-01-06 16:06:24 +0700

    Add files via upload

---
+++ Mencari Titik Beku/TitikBeku.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+    int a;
+    cout<<"MASUKAN SUHU : "; cin>>a;
+    cout<<"\n==========================================\n";
+    if (a<0)
+
+        cout<<"SUHU DARI "<<a<<" DERAJAT ADALAH ZAT BEKU ";
+
+    else
+
+        cout<<"SUHU DARI "<<a<<" DERAJT ADALAH CAIR ";
+
+
+
+
+
+    getch();
+    return 0;
+}
